f0365a97f9f96f516756b86afcb0310146bed9eb
ykmshhh <yakukhno.maria@lll.kpi.ua>
2025-10-03 19:38:24 +0300

    add Java models for ER diagram

---
+++ models/Account.java
@@ -0,0 +1,58 @@
+package models;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private int id;
+    private String IBAN;
+    private BigDecimal balance;
+    private String currency;
+    private String type;
+
+    public Account(int id, String IBAN, BigDecimal balance, String currency, String type) {
+        this.id = id;
+        this.IBAN = IBAN;
+        this.balance = balance;
+        this.currency = currency;
+        this.type = type;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIBAN() {
+        return IBAN;
+    }
+
+    public void setIBAN(String IBAN) {
+        this.IBAN = IBAN;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
